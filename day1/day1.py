643dc3fdaef6aa59a07c176c64a473b954cfd503
moink <theresa.robinson@gmail.com>
2021-12-01 06:39:56 +0100

    Day 1 cleaned up

--- day1/day1.py
+++ day1/day1.py
@@ -1,48 +1,26 @@
-import contextlib
-import collections
-import copy
-import functools
-import itertools
-import math
-import numpy as np
 import pandas as pd
-import re
 
 import advent_tools
 
 
 def main():
-    # advent_tools.TESTING = True
-    data = advent_tools.read_all_integers()
-    # data = advent_tools.read_whole_input()
-    # data = advent_tools.read_input_lines()
-    # data = advent_tools.read_input_line_groups("\n\n")
-    # data = advent_tools.read_input_no_strip()
-    # data = advent_tools.read_dict_from_input_file(sep=' => ', key='left')
-    # data = advent_tools.read_dict_of_list_from_file(sep=' => ', key='left')
-    # data = advent_tools.read_one_int_per_line()
-    # data = advent_tools.PlottingGrid.from_file({'.' : 0, '#' : 1})
-    # data = advent_tools.read_input_line_groups()
+    data = advent_tools.read_one_int_per_line()
     data = process_input(data)
     print('Part 1:', run_part_1(data))
     print('Part 2:', run_part_2(data))
 
 
 def process_input(data):
-    data = pd.Series(q[0] for q in data)
-    return data
+    return pd.Series(data)
 
 
 def run_part_1(data):
-    d = data.diff()
-    return sum(d>0)
-    pass
+    return sum(data.diff() > 0)
 
 
 def run_part_2(data):
-    s = data.rolling(3).sum()
-    d = s.diff()
-    return sum(d > 0)
+    rolling = data.rolling(3).sum()
+    return sum(rolling.diff() > 0)
 
 
 if __name__ == '__main__':
