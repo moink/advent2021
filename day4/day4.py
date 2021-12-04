b2f32ca7a0f07e0c63c0539d51a78fd5c521ac06
moink <theresa.robinson@gmail.com>
2021-12-04 06:49:02 +0100

    day 4 cleaned up

--- day4/day4.py
+++ day4/day4.py
@@ -1,28 +1,11 @@
-import contextlib
-import collections
-import copy
-import functools
-import itertools
-import math
-from io import FileIO, StringIO
+from io import StringIO
 
 import numpy as np
-import pandas as pd
-import re
 
 import advent_tools
 
 
 def main():
-    # advent_tools.TESTING = True
-    # data = advent_tools.read_all_integers()
-    # data = advent_tools.read_whole_input()
-    # data = advent_tools.read_input_lines()
-    # data = advent_tools.read_input_no_strip()
-    # data = advent_tools.read_dict_from_input_file(sep=' => ', key='left')
-    # data = advent_tools.read_dict_of_list_from_file(sep=' => ', key='left')
-    # data = advent_tools.read_one_int_per_line()
-    # data = advent_tools.PlottingGrid.from_file({'.' : 0, '#' : 1})
     data = advent_tools.read_input_line_groups()
     drawing, boards = process_input(data)
     print('Part 1:', run_part_1(drawing, boards))
@@ -40,42 +23,41 @@ def process_input(data):
 
 def is_winner(drawn):
     size, _ = drawn.shape
-    row_sums = drawn.sum(axis=1)
-    # print(row_sums)
-    if (row_sums == size).any():
-        return True
-    col_sums = drawn.sum(axis=0)
-    if (col_sums == size).any():
-        return True
+    for axis in [0, 1]:
+        row_sums = drawn.sum(axis=axis)
+        if (row_sums == size).any():
+            return True
     return False
 
 
 def score_board(board, drawn, cur_draw):
-    to_sum = board * (1 - drawn)
-    sum = to_sum.sum().sum()
+    not_called = board * (1 - drawn)
+    sum = not_called.sum().sum()
     return sum * cur_draw
 
+
 def run_part_1(drawing, boards):
-    drawns = [np.zeros_like(boards[0])] * len(boards)
+    drawn = [np.zeros_like(boards[0])] * len(boards)
     for cur_draw in drawing:
         for board_num in range(len(boards)):
-            drawns[board_num] = drawns[board_num] | (boards[board_num] == cur_draw)
-            if is_winner(drawns[board_num]):
-                return score_board(boards[board_num], drawns[board_num], cur_draw)
+            drawn[board_num] = drawn[board_num] | (boards[board_num] == cur_draw)
+            if is_winner(drawn[board_num]):
+                return score_board(boards[board_num], drawn[board_num], cur_draw)
     raise RuntimeError("No winner after all drawings")
 
+
 def run_part_2(drawing, boards):
-    drawns = [np.zeros_like(boards[0])] * len(boards)
+    drawn = [np.zeros_like(boards[0])] * len(boards)
     no_win_yet = set(range(len(boards)))
     for cur_draw in drawing:
         for board_num in range(len(boards)):
             if board_num in no_win_yet:
-                drawns[board_num] = drawns[board_num] | (boards[board_num] == cur_draw)
-                if is_winner(drawns[board_num]):
+                drawn[board_num] = drawn[board_num] | (boards[board_num] == cur_draw)
+                if is_winner(drawn[board_num]):
                     no_win_yet.remove(board_num)
                 if len(no_win_yet) == 0:
-                    return score_board(boards[board_num], drawns[board_num], cur_draw)
-    raise RuntimeError("No winner after all drawings")
+                    return score_board(boards[board_num], drawn[board_num], cur_draw)
+    raise RuntimeError("Still some losing boards after all drawings")
 
 
 if __name__ == '__main__':
