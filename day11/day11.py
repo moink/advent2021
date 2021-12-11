aaf10ce35a88eb02a7927b532d94148d0458516f
moink <theresa.robinson@gmail.com>
2021-12-11 06:45:29 +0100

    combine both parts

--- day11/day11.py
+++ day11/day11.py
@@ -6,48 +6,38 @@ import advent_tools
 
 def main():
     data = advent_tools.read_nparray_from_digits()
-    print('Part 1:', run_part_1(data))
-    print('Part 2:', run_part_2(data))
+    part1, part2 = run_both_parts(data)
+    print('Part 1:', part1)
+    print('Part 2:', part2)
 
 
-def run_part_1(energy):
+def run_both_parts(energy):
+    new_energy = energy
+    new_flashes = np.zeros_like(energy)
+    part1_sol = 0
     flash_count = 0
-    n_steps = 100
     convolve_matrix = np.ones((3, 3))
-    convolve_matrix[1, 1] = 0
-    for step in range(n_steps):
+    step = 0
+    while True:
+        step = step + 1
         energy = energy + 1
         flashes = energy > 9
-        need_to_continue = True
-        while need_to_continue:
-            neighbour_flashes = signal.convolve(flashes, convolve_matrix, mode='same').round(0).astype(int)
+        have_new_flashes = True
+        while have_new_flashes:
+            neighbour_flashes = (signal.convolve(flashes, convolve_matrix, mode='same')
+                                 .round(0).astype(int))
             new_energy = energy + neighbour_flashes
             new_flashes = new_energy > 9
-            need_to_continue = (new_flashes & ~flashes).sum().sum() > 0
+            have_new_flashes = (new_flashes & ~flashes).sum().sum() > 0
             flashes = new_flashes
         energy = new_energy
         energy[flashes] = 0
         flash_count += new_flashes.sum().sum()
-    return flash_count
-
-def run_part_2(energy):
-    n_steps = 1000
-    convolve_matrix = np.ones((3, 3))
-    convolve_matrix[1, 1] = 0
-    for step in range(n_steps):
-        energy = energy + 1
-        flashes = energy > 9
-        need_to_continue = True
-        while need_to_continue:
-            neighbour_flashes = signal.convolve(flashes, convolve_matrix, mode='same').round(0).astype(int)
-            new_energy = energy + neighbour_flashes
-            new_flashes = new_energy > 9
-            need_to_continue = (new_flashes & ~flashes).sum().sum() > 0
-            flashes = new_flashes
-        energy = new_energy
-        energy[flashes] = 0
+        if step == 100:
+            part1_sol = flash_count
         if flashes.all().all():
-            return step + 1
+            return part1_sol, step
+
 
 
 if __name__ == '__main__':
