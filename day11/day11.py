5414b9965134083b81f99221c6b8f74bad008799
moink <theresa.robinson@gmail.com>
2021-12-11 06:23:14 +0100

    day 11 cleaned up a little

--- day11/day11.py
+++ day11/day11.py
@@ -1,31 +1,10 @@
-import contextlib
-import collections
-import copy
-import functools
-import itertools
-import math
-import re
-import statistics
-
 import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
 from scipy import signal
 
 import advent_tools
 
 
 def main():
-    # advent_tools.TESTING = True
-    # data = advent_tools.read_all_integers()
-    # data = advent_tools.read_whole_input()
-    # data = advent_tools.read_input_lines()
-    # data = advent_tools.read_input_no_strip()
-    # data = advent_tools.read_dict_from_input_file(sep=' => ', key='left')
-    # data = advent_tools.read_dict_of_list_from_file(sep=' => ', key='left')
-    # data = advent_tools.read_one_int_per_line()
-    # data = advent_tools.PlottingGrid.from_file({'.' : 0, '#' : 1})
-    # data = advent_tools.read_input_line_groups()
     data = advent_tools.read_nparray_from_digits()
     data = process_input(data)
     print('Part 1:', run_part_1(data))
@@ -33,7 +12,6 @@ def main():
 
 
 def process_input(data):
-    # print(data)
     return data
 
 
@@ -58,7 +36,6 @@ def run_part_1(energy):
     return flash_count
 
 def run_part_2(energy):
-    flash_count = 0
     n_steps = 1000
     convolve_matrix = np.ones((3, 3))
     convolve_matrix[1, 1] = 0
@@ -74,7 +51,6 @@ def run_part_2(energy):
             flashes = new_flashes
         energy = new_energy
         energy[flashes] = 0
-        flash_count += new_flashes.sum().sum()
         if flashes.all().all():
             return step + 1
 
