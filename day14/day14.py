71bb8b675f26fe02caf90b171a2b5fb1575ffbeb
moink <theresa.robinson@gmail.com>
2021-12-14 06:47:36 +0100

    day 14 cleaned up

--- day14/day14.py
+++ day14/day14.py
@@ -1,55 +1,38 @@
-import contextlib
 import collections
-import copy
-import functools
-import itertools
-import math
-import re
-import statistics
-
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
 
 import advent_tools
 
 
 def main():
-    # advent_tools.TESTING = True
-    # data = advent_tools.read_all_integers()
-    # data = advent_tools.read_whole_input()
-    # data = advent_tools.read_input_lines()
-    # data = advent_tools.read_input_no_strip()
-    data = advent_tools.read_dict_from_input_file(sep=' -> ', key='left')
-    # data = advent_tools.read_dict_of_list_from_file(sep=' => ', key='left')
-    # data = advent_tools.read_one_int_per_line()
-    # data = advent_tools.PlottingGrid.from_file({'.' : 0, '#' : 1})
-    # data = advent_tools.read_input_line_groups()
-    # data = advent_tools.read_nparray_from_digits()
-    if advent_tools.TESTING:
-        start = "NNCB"
-    else:
-        start = "OFSVVSFOCBNONHKFHNPK"
-    print('Part 1:', run_part(start, data, 10))
-    print('Part 2:', run_part(start, data, 40))
+    (polymer, *_), map_part = advent_tools.read_input_line_groups()
+    insertion_map = read_insertion_map(map_part)
+    print('Part 1:', run_part(polymer, insertion_map, 10))
+    print('Part 2:', run_part(polymer, insertion_map, 40))
 
 
-def run_part(polymer, data, steps):
+def read_insertion_map(map_part):
+    insertion_map = {}
+    for line in map_part:
+        left, right = line.split(' -> ')
+        insertion_map[left.strip()] = right.strip()
+    return insertion_map
+
+
+def run_part(polymer, insertion_map, steps):
     pair_counts = collections.defaultdict(int)
     char_counts = collections.defaultdict(int, collections.Counter(polymer))
     for first_char, second_char in zip(polymer[:-1], polymer[1:]):
-        pair_counts["".join((first_char, second_char))] +=1
+        pair_counts["".join((first_char, second_char))] += 1
     for _ in range(steps):
         new_pair_counts = collections.defaultdict(int)
         for pair, count in pair_counts.items():
-            between = data[pair]
             first_char, second_char = pair
+            between = insertion_map[pair]
             new_pair_counts["".join((first_char, between))] += count
             new_pair_counts["".join((between, second_char))] += count
             char_counts[between] += count
         pair_counts = new_pair_counts
     return max(char_counts.values()) - min(char_counts.values())
-    # not 4302675529691
 
 if __name__ == '__main__':
     main()
