24a0e603afcd43b40e8f043f004643e307c9ef87
moink <theresa.robinson@gmail.com>
2021-12-15 08:23:41 +0100

    variable name changes

--- day15/day15.py
+++ day15/day15.py
@@ -4,39 +4,39 @@ import advent_tools
 
 
 def main():
-    data = advent_tools.read_nparray_from_digits()
-    print('Part 1:', run_part_1(data))
-    print('Part 2:', run_part_2(data))
+    risk_level = advent_tools.read_nparray_from_digits()
+    print('Part 1:', run_part_1(risk_level))
+    print('Part 2:', run_part_2(risk_level))
 
 
-def run_part_1(data):
-    return find_min_path(data)
+def run_part_1(risk_level):
+    return find_min_path(risk_level)
 
 
-def run_part_2(data):
+def run_part_2(risk_level):
     return find_min_path(
         np.concatenate(
             [np.concatenate(
-                [np.mod(data + i + j - 1, 9) + 1 for j in range(5)], 1)
+                [np.mod(risk_level + i + j - 1, 9) + 1 for j in range(5)], 1)
                 for i in range(5)], 0))
 
 
-def find_min_path(data):
-    big_number = data.sum().sum()
-    cost = big_number * np.ones_like(data)
-    cost[- 1, - 1] = data[- 1, - 1]
-    keep_going = True
-    while keep_going:
+def find_min_path(risk_level):
+    big_number = risk_level.sum().sum()
+    cost = big_number * np.ones_like(risk_level)
+    cost[- 1, - 1] = risk_level[- 1, - 1]
+    not_converged = True
+    while not_converged:
         old_cost = cost
         cost = np.minimum.reduce([
             cost,
-            data + shift_with_padding(cost, -1, 0, big_number),
-            data + shift_with_padding(cost, 1, 0, big_number),
-            data + shift_with_padding(cost, -1, 1, big_number),
-            data + shift_with_padding(cost, 1, 1, big_number)
+            risk_level + shift_with_padding(cost, -1, 0, big_number),
+            risk_level + shift_with_padding(cost, 1, 0, big_number),
+            risk_level + shift_with_padding(cost, -1, 1, big_number),
+            risk_level + shift_with_padding(cost, 1, 1, big_number)
         ])
-        keep_going = np.abs(cost - old_cost).any().any()
-    return cost[0, 0] - data[0, 0]
+        not_converged = np.abs(cost - old_cost).any().any()
+    return cost[0, 0] - risk_level[0, 0]
 
 
 def shift_with_padding(data, shift, axis, pad_value):
