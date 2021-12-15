dfe6211a1c07bc1997214404622de34dac14fe0c
moink <theresa.robinson@gmail.com>
2021-12-15 07:54:56 +0100

    make it all ints by not needing to use inf anymore

--- day15/day15.py
+++ day15/day15.py
@@ -4,7 +4,7 @@ import advent_tools
 
 
 def main():
-    data = advent_tools.read_nparray_from_digits().astype(float)
+    data = advent_tools.read_nparray_from_digits()
     print('Part 1:', run_part_1(data))
     print('Part 2:', run_part_2(data))
 
@@ -22,21 +22,22 @@ def run_part_2(data):
 
 
 def find_min_path(data):
-    cost = np.inf * np.ones_like(data)
+    big_number = data.sum().sum()
+    cost = big_number * np.ones_like(data)
     max_y, max_x = data.shape
     cost[max_y - 1, max_x - 1] = data[max_y - 1, max_x - 1]
     keep_going = True
     while keep_going:
         old_cost = cost
-        cost = np.minimum(cost, data + shift_padding_with_inf(cost, -1, 0))
-        cost = np.minimum(cost, data + shift_padding_with_inf(cost, 1, 0))
-        cost = np.minimum(cost, data + shift_padding_with_inf(cost, -1, 1))
-        cost = np.minimum(cost, data + shift_padding_with_inf(cost, 1, 1))
+        cost = np.minimum(cost, data + shift_with_padding(cost, -1, 0, big_number))
+        cost = np.minimum(cost, data + shift_with_padding(cost, 1, 0, big_number))
+        cost = np.minimum(cost, data + shift_with_padding(cost, -1, 1, big_number))
+        cost = np.minimum(cost, data + shift_with_padding(cost, 1, 1, big_number))
         keep_going = np.abs(cost - old_cost).any().any()
     return int(cost[0, 0] - data[0, 0])
 
 
-def shift_padding_with_inf(data, shift, axis):
+def shift_with_padding(data, shift, axis, pad_value):
     shifted_data = np.roll(data, shift, axis=axis)
     null_slice = slice(None, None)
     if shift < 0:
@@ -47,7 +48,7 @@ def shift_padding_with_inf(data, shift, axis):
         full_slice = (null_slice, part_slice)
     else:
         full_slice = (part_slice, null_slice)
-    shifted_data[full_slice] = np.inf
+    shifted_data[full_slice] = pad_value
     return shifted_data
 
 
