a3ac4b28c66e960332e5d43823dcd0c886aadc34
moink <theresa.robinson@gmail.com>
2021-12-20 08:13:06 +0100

    day 20 cleaned up

--- day20/day20.py
+++ day20/day20.py
@@ -1,44 +1,24 @@
-import contextlib
-import collections
-import copy
-import functools
-import itertools
-import math
-import re
-import statistics
-
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
-
 import advent_tools
 
 
 def main():
     data = advent_tools.read_input_line_groups()
-    data = process_input(data)
-    print('Part 1:', run_part(data, 2))
-    print('Part 2:', run_part(data, 50))  # 20767 is too high
+    grid, algo = process_input(data)
+    print('Part 1:', run_part(grid, algo, 2))
+    print('Part 2:', run_part(grid, algo, 50))
 
 
 def process_input(data):
     algo = [0 if char == "." else 1 for char in data[0][0]]
     grid = advent_tools.PlottingGrid.from_str(text=data[1])
-    return algo, grid
+    return grid, algo
 
 
-def get_new_shape(shape):
-    n, m = shape
-    return n + 2, m + 2
-
-
-def run_part(data, steps):
-    algo, grid = data
+def run_part(grid, algo, steps):
     off_edges_val = 0
     for _ in range(steps):
-        # grid.show()
-        new_grid = advent_tools.PlottingGrid(get_new_shape(grid.grid.shape))
         n, m = grid.grid.shape
+        new_grid = advent_tools.PlottingGrid((n + 2, m + 2))
         for j in range(-1, n + 1):
             for i in range(-1, m + 1):
                 nums = []
@@ -56,11 +36,9 @@ def run_part(data, steps):
             off_edges_val = algo[0]
         else:
             off_edges_val = algo[-1]
+        grid.draw()
     grid.show()
     return grid.sum().sum()
-
-def run_part_2(data):
-    pass
 
 
 if __name__ == '__main__':
